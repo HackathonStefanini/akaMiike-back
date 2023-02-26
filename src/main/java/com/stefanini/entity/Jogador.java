0c333f50dbfccb0be6865fdd83632a473e133469
akaMiike <mike.nss@alu.ufc.br>
2023-02-26 14:08:33 -0300

    fix: bug corrections in initial files

--- src/main/java/com/stefanini/entity/Jogador.java
+++ src/main/java/com/stefanini/entity/Jogador.java
@@ -5,6 +5,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 @Table(schema = "tb_jogador")
 public class Jogador {
 
@@ -30,5 +31,53 @@ public class Jogador {
     private List<Stefamon> stefamons = new ArrayList<>();
 
     public Jogador() {
+    }
+
+    public Jogador(String nickname, String password, BigDecimal saldo, List<Stefamon> stefamons) {
+        this.id = id;
+        this.nickname = nickname;
+        this.password = password;
+        this.saldo = saldo;
+        this.stefamons = stefamons;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public BigDecimal getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(BigDecimal saldo) {
+        this.saldo = saldo;
+    }
+
+    public List<Stefamon> getStefamons() {
+        return stefamons;
+    }
+
+    public void setStefamons(List<Stefamon> stefamons) {
+        this.stefamons = stefamons;
     }
 }
